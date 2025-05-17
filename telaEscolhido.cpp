de7e2df2de410c4907e35b27c4628cc8aa306662
Danielsps <danielsoares105@gmail.com>
2025-05-17 13:48:34 -0300

    Alteração de design na tela final de filme escolhido

--- telaEscolhido.cpp
+++ telaEscolhido.cpp
@@ -4,10 +4,10 @@
 
 telaEscolhido::telaEscolhido(wxWindow* parent, filmeInfos containerFilme) : wxPanel(parent), containerFilme(containerFilme) {
 	SetSize(1600, 800);
-	SetBackgroundColour(wxColour(20, 24, 28));
 
-	ImagePanel* posterFilme = new ImagePanel(this, containerFilme.getPoster(), wxPoint(1050, 50), wxSize(500, 700));
+	ImagePanel* fimFundo = new ImagePanel(this, "src/fimFundoCompleto.png", wxPoint(0, 0), wxSize(1600, 800));
 
-	ImagePanel* resultadoFilme = new ImagePanel(this, "src/fim.png", wxPoint(50, 50), wxSize(950, 700));
+	ImagePanel* bordaPoster = new ImagePanel(this, "src/fimBordaPoster.png", wxPoint(1050, 50), wxSize(500, 700));
+	ImagePanel* posterFilme = new ImagePanel(this, containerFilme.getPoster(), wxPoint(1100, 100), wxSize(400, 600));	
 
 }
